b0007a86806e455f25ff19c73ba80adc4f7e64b6
Kithikan <kithikan30@gmail.com>
2026-07-22 11:41:47 +0530

    Create If_Marks.java

---
+++ If_Marks.java
@@ -0,0 +1,12 @@
+class If_Marks {
+	public static void main(String args[]) {
+		 
+		int student_marks = 30;
+		
+		if (student_marks >= 50) {
+			System.out.println("Pass");
+			
+		}else 
+			System.out.println("Fail");
+	}
+}	
