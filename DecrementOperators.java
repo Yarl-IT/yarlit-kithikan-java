d8044759555ae5286467d226190d4a75b0d11fdc
Kithikan <kithikan30@gmail.com>
2026-07-22 10:42:08 +0530

    Add DecrementOperators demo
    Add a new Java class demonstrating various decrement operators including post-decrement (y--), pre-decrement (--y), and compound assignment (y-=).

---
+++ DecrementOperators.java
@@ -0,0 +1,18 @@
+class DecrementOperators {
+	public static void main(String args[]){
+		int y = 15;
+		y = y - 5;
+		System.out.println(y);
+		
+		y -= 5;
+		System.out.println(y);
+		
+		y--;						// javapost-decrement
+		System.out.println(y);
+
+		--y;						// pre-decrement
+		System.out.println(y);
+		System.out.println(y--);
+		System.out.println(y);
+		System.out.println(--y);
+	}	
